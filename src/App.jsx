f7bef3e20aec4cf35ac8d8c2a0d514583d76eda3
zura tetrashvili <zuratetra@gmail.com>
2025-02-26 20:55:30 +0400

    finish markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="bg-white py-0 md:py-[65px] pr-[95px] relative">
+      <div className="bg-white py-0 md:py-[65px] pr-0 md:pr-[95px] min-w-[327px] rounded-lg md:flex relative md:gap-10">
         <div className="">
-          <div className="">
-            <img src="/mention.svg" alt="mention image" />
+          <div className="flex-justify-center hidden md:block">
+            <img src="/mention-half.svg" alt="mention image" />
           </div>
-          <div className="absolute top-[50px] md:top-[220px] md:left-[-70px]">
-            <img className="w-[53px] md:w-[150px]" src="/box.svg" alt="" />
+          <div className="block md:hidden absolute top-[-110px] left-10">
+            <img src="/mention-full.png" alt="mention image" />
+          </div>
+          <div className="absolute top-[-30px] left-[35px] md:top-[220px] md:left-[-70px]">
+            <img
+              src="/box.svg"
+              alt="box image"
+              className="w-[80px] md:w-[150px]"
+            />
           </div>
         </div>
-        <div className="">
-          <h1 className="w=full">FAQ</h1>
+        <div className="mt-40">
+          <h1 className="">FAQ</h1>
+          <div className="flex items-center justify-center w-full gap-4 mt-10">
+            <p className="flex items-center">
+              How many team members can I invite?
+            </p>
+            <img src="/arrow.png" alt="arrow" className="w-4 h-2" />
+          </div>
+          <p className="mt-3">
+            No more than 1GB. All files in your account must fit your allotted
+            storage space.
+          </p>
         </div>
       </div>
     </>
