0480fd8443c392e584c4471ecc192b3f6f834b6f
zura tetrashvili <zuratetra@gmail.com>
2025-02-28 00:14:58 +0400

    markup bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-white py-0 md:py-[65px] pr-0 md:pr-[95px] min-w-[327px] rounded-lg md:flex relative md:gap-[88px] p-5 md:p-0">
+      <div className="bg-white py-0 md:py-[65px] md:pr-[95px] min-w-[327px] rounded-lg md:flex relative md:gap-[88px] p-5 md:p-0">
         <div className="">
           <div className="hidden md:block">
             <img src="/mention-half.svg" alt="mention image" />
@@ -26,14 +26,23 @@ function App() {
         <div className="mt-40 md:mt-0">
           <h1 className="text-[32px] font-bold flex justify-start">FAQ</h1>
           {info.map((item, index) => (
-            <div key={index} className="">
+            <div
+              key={index}
+              className="max-w-[333px] border-b-1 border-gray-300 pb-2"
+            >
               <div
-                onClick={() => setAccord(item.id)}
-                className="flex items-center mt-5 cursor-pointer"
+                onClick={() => {
+                  if (accord === item.id) {
+                    setAccord(0);
+                  } else {
+                    setAccord(item.id);
+                  }
+                }}
+                className="flex items-center mt-5 cursor-pointer md:w-[350px] justify-between"
               >
                 <p
                   style={{ fontWeight: accord === item.id ? "bold" : "normal" }}
-                  className="pt-2 text-left"
+                  className="pt-2 text-left text-sm"
                 >
                   {item.question}
                 </p>
@@ -41,13 +50,17 @@ function App() {
                   src="/arrow.png"
                   alt="arrow image"
                   className="w-4 h-3 ml-4"
+                  style={{
+                    transform:
+                      accord === item.id ? "rotate(180deg)" : "rotate(0deg)",
+                  }}
                 />
               </div>
               {accord === item.id ? (
-                <p className="pt-2 text-left">{item.answer}</p>
-              ) : (
-                ""
-              )}
+                <p className="pt-2 text-left text-xs text-gray-600 w-[280px] md:w-auto">
+                  {item.answer}
+                </p>
+              ) : null}
             </div>
           ))}
         </div>
