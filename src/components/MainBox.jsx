17ce75694590c7f02793ac9910436c60fe3549db
zura tetrashvili <zuratetra@gmail.com>
2025-03-01 16:00:33 +0400

    markup bug fixing

--- src/components/MainBox.jsx
+++ src/components/MainBox.jsx
@@ -7,7 +7,7 @@ function MainBox() {
 
   return (
     <div>
-      <div className="mt-40 md:mt-0 mb-10 md:mb-0">
+      <div className="mt-40 md:mt-0 mb-10 md:mb-0 h-[375px]">
         <h1 className="text-[32px] font-bold flex justify-start">FAQ</h1>
         {info.map((item, index) => (
           <div
